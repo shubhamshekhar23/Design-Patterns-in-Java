dc63b948698b00246fbfffed2404eb4141c0fabd
shubhamshekhar23 <shubham.ae.iitkgp@gmail.com>
2020-12-25 11:59:46 +0530

    command and chainofresp updated

--- Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibility.java
+++ Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibility.java
@@ -3,16 +3,21 @@ class ChainOfResponsibility
 	public static void main(String[] args)
 	{	
         try{
+            /* Creating a new virus instance and set the severity level just to test */
             CarAiVirus virus = new CarAiVirus(1);
 
+            /* Creating testers instance */
             Engineer e1 = new Engineer();
             TeamLead t1 = new TeamLead();
             TechnicalOfficer ct1 = new TechnicalOfficer();
             Expert ee = new Expert();
 
+            /* Set the hierarchy by using ofr each one by passing the instance of upper level tester */
             e1.setNextDebugger(t1);
             t1.setNextDebugger(ct1);
             ct1.setNextDebugger(ee);
+
+            /* Give the virus to the lowest level tester */
             e1.debug(virus);
         }
         catch(Exception e){
--- Behavioral Design Patterns/Command/Command.java
+++ Behavioral Design Patterns/Command/Command.java
@@ -3,12 +3,32 @@ class Command
 	public static void main(String[] args)
 	{	
         try{
+            /* Setting up of system */
             Remote r = new Remote();
             MusicSystem ms = new MusicSystem();
+            AirCondition ac = new AirCondition();
+            LightControl lc = new LightControl();
+
+            /* setting up instance of all the command that has to perform and pass the system instance */
             CommandPlan musicplay = new MusicPlay(ms);
+            CommandPlan lightOn = new LightOn(lc);
+            CommandPlan lightOff = new LightOff(lc);
+            CommandPlan acon = new AcOn(ac);
+            CommandPlan acoff = new AcOff(ac);
+
+
+            /* Everytime we want to change the tash that remote has to perform, we use setCommand and pass the commandInstance */
             r.setCommand(musicplay);
             r.buttonPressed();
-            r.setCommand(new LightOn(new LightControl()));
+
+            r.setCommand(lightOn);
+            r.buttonPressed();
+            r.setCommand(lightOff);
+            r.buttonPressed();
+
+            r.setCommand(acon);
+            r.buttonPressed();
+            r.setCommand(acoff);
             r.buttonPressed();
         }
         catch(Exception e){
@@ -17,6 +37,6 @@ class Command
 	}
 }
 
-/*Here in this example we only have one button in the remote that can be used to perform differnt task like lighton , play miusic etc.
-All these commandds can be performed on a single button press; we can do it by creating a class out of thses commands
-which uses the original system methods( like Lightcontrol, muysicsystem) and then perform on execute method */
+/*Here in this example we only have one button in the remote that can be used to perform differnt tasks like lighton, play miusic, turn on AC.
+All these commands can be performed on a single button press; we just have to setCommand and everytime we want it to do adifferent task, we will pass the instance of the new task. we can do it by creating a class out of thses commands
+which uses the original system methods( like Lightcontrol, muysicsystem, aircondition) and then perform on execute method */
