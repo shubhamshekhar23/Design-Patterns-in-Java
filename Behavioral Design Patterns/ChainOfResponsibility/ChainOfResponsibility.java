b13f029188139e23df907ce06feb2a79cf240af6
shubhamshekhar23 <shubham.ae.iitkgp@gmail.com>
2020-12-25 11:35:34 +0530

    chain of responsibility updated

--- Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibility.java
+++ Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibility.java
@@ -21,4 +21,4 @@ class ChainOfResponsibility
 	}
 }
 
-/* There are new viruses being discovered in the system whose severity level can be set ; now there is a debugger chain hierecahy; if an engineer can solve it then it's fine; otherwise he will pass it to team lead; if team lead can't solve it then he will pass it to technical officer;etc. So a chain abstract class is created and all the debuggers will extend from it amd create a chain */
+/* There are new viruses being discovered in the system whose severity level can be set ; now there is a debugger chain hierecahy; if an engineer can solve it then it's fine; otherwise he will pass it to team lead; if team lead can't solve it then he will pass it to technical officer;if he can't then he will pass it to the Expert where its solved finally. So a chain abstract class is created and all the debuggers will extend from it amd create a chain */
